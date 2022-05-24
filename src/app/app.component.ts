755b25f3f7de2f2efb7ef06f34a1c52fcf775b95
WhitenAND07 <jordi14and@gmail.com>
2022-05-24 16:34:11 +0200

    Created new module dbz and main page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public title: string = '01-bases';
+  public title: string = 'Angular-bases';
   
 }
