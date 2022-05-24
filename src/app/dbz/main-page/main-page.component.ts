752bcd402b2e7fd7bef92d07ebb52cf8dfac6ba8
WhitenAND07 <jordi14and@gmail.com>
2022-05-24 17:37:49 +0200

    Work about lists and forms

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,10 +1,45 @@
 import { Component} from '@angular/core';
 
+interface Personaje {
+  nombre:string;
+  poder:number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
   
-  agregar(): void {}
+  public personajes: Personaje[] = [
+    {
+      nombre: 'Krillin',
+      poder: 700
+    },
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
+
+  public nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  agregar(): void {
+    if(this.nuevo.nombre.trim().length === 0){ return}
+      
+    this.personajes.push(this.nuevo);
+    
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+  }
+
 }
