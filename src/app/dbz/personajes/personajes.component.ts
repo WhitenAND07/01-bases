5ee3861c6085ef8f57835f42b9b95a90070b026b
WhitenAND07 <jordi14and@gmail.com>
2022-05-24 17:52:49 +0200

    Work about components child & @Input

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 
 @Component({
@@ -6,5 +7,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './personajes.component.html'
 })
 export class PersonajesComponent {
-  personajes: any[] = [];
+  @Input() personajes: Personaje[] = [];
 }
