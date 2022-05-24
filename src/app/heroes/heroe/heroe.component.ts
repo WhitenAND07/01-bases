8421f2a11bdf88f07250db9a9e6fff4f2b8fbbc8
WhitenAND07 <jordi14and@gmail.com>
2022-05-24 11:11:35 +0200

    Work about component Heroe

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
 
+import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-heroe',
@@ -13,6 +13,7 @@ export class HeroeComponent{
         return this.nombre.toUpperCase();
     }
 
+    
     obtenerNombre(): string{
         //return this.nombre + ' - ' + this.edad;
         return `${this.nombre} - ${this.edad}`;
